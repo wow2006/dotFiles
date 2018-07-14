8297719cf4428920463ccc37d4fd80c60582aef3
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-07-14 22:13:43 +0200

    Add cppcheck

--- static.py
+++ static.py
@@ -1,104 +1,134 @@
-import neovim, tempfile
-from os import chdir, curdir, remove
-from os.path import dirname, exists, isdir, join, abspath
+from os import chdir, remove
+from os.path import exists, isdir, abspath, join, dirname, basename, splitext
 import subprocess
+import json
+import neovim
 
 
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
-        self.buildDir = ''
-        self.tempFile = join(tempfile.gettempdir(),
-                             tempfile.gettempprefix() + "_analysis")
-        self.commands = {
-            "clang": "clang-tidy -format-style=file -p {} -checks={} {}",
-            "pvs": "pvs-studio-analyzer analyze",
-            "pvs_ans": "plog-converter -a %s -t errorfile PVS-Studio.log",
-            "oclint": lambda filename, args: "%s %s" % (args, filename),
-            "cppcheck": lambda filename, args: "%s %s" % (args, filename),
-        }
+        self.build_dir = ''
+
+        config_file = join(dirname(abspath(__file__)), 'static.json')
+        with open(config_file, 'r') as file_handler:
+            self.commands = json.loads(file_handler.read())
 
     def __del__(self):
-        remove(self.tempFile)
+        for key in self.commands:
+            temp_file = self.commands[key].get('tempFile')
+            if temp_file:
+                remove(temp_file)
 
     @neovim.command("SetBuild", range='', nargs='*')
-    def setBuild(self, args, range):
+    def set_build(self, args, range):
         if (not range and not isdir(args[0])):
             self.vim.out_write("Please write build dir")
             return
 
-        self.buildDir = abspath(args[0])
-        self.vim.out_write("Set build dir to {}\n".format(self.buildDir))
+        self.build_dir = abspath(args[0])
+        self.vim.out_write("Set build dir to {}\n".format(self.build_dir))
 
     @neovim.command("AnalysisClang", range='', nargs='*')
     def analysisClang(self, args, range):
-        currentFile = self.vim.current.buffer.name
-        self.vim.out_write("current %s\n" % currentFile)
-        if (not currentFile):
+        if not self.check_build_dir():
             return
 
-        if (not ".cpp" in currentFile):
-            return
+        current_file = self.vim.current.buffer.name
 
-        #command = self.commands["clang"] % (self.tempFile, '*', currentFile)
-        #self.vim.out_write("command %s" % command)
-        #result  = self.runCommand(command)
-        #if(not result):
-        #    return
+        command = self.commands["clang-tidy"]
 
-        #errors = result.stdout.decode("utf-8")
+        if command["needBuild"]:
+            chdir(self.build_dir)
 
-        #self.writeToQuickFix(errors, currentFile)
+        result = self.runCommand(command["command"][0] % (command["tempFile"],
+                                                          '*', current_file))
 
-    @neovim.command("AnalysisPVS", range='', nargs='*')
-    def analysisPVS(self, args, range):
-        if(not self.buildDir or not exists(self.buildDir)):
-            if(exists('build/')):
-                if(exists('Debug/')):
-                    self.buildDir = "build/Debug/"
-                elif(exists('Release/')):
-                    self.buildDir = "build/Release/"
-            self.vim.out_write("Please set Build dir\n")
-            self.vim.out_write("Using :SetBuild \"build\"\n")
-            return
-
-        currentFile = self.vim.current.buffer.name
-        if (not currentFile or not (".cpp" in currentFile)):
-            return
-        self.vim.out_write("current %s\n" % currentFile)
-
-        command = self.commands["pvs"]
-
-        chdir(self.buildDir)
-        if(not exists('PVS-Studio.log')):
-            self.runCommand(command)
-
-        command = self.commands["pvs_ans"] % "GA:1,2,3"
-
-        result = self.runCommand(command)
-
-        if(not result):
-            self.vim.out_write("Error with %s\n" % currentFile)
+        if not result:
+            self.vim.out_write("Error with %s\n" % current_file)
             return
 
         result = result.stdout.decode("utf-8").split('\n')
 
-        errors = "\n".join([x for x in result if currentFile in x])
-        
-        self.writeToQuickFix(errors, '/tmp/pvs')
+        errors = "\n".join(
+            [x for x in result if splitext(basename(current_file))[0] in x])
+
+        self.writeToQuickFix(errors, command["tempFile"])
+
+    @neovim.command("AnalysisPVS", range='', nargs='*')
+    def analysis_pvs(self, args, range):
+        if not self.check_build_dir():
+            return
+
+        current_file = self.vim.current.buffer.name
+        if not current_file or not (".cpp" in current_file):
+            return
+
+        self.vim.out_write("current %s\n" % current_file)
+
+        pvs_values = self.commands["pvs"]
+        command = pvs_values["command"][0]
+
+        if command["needBuild"]:
+            chdir(self.build_dir)
+
+        if not exists('PVS-Studio.log'):
+            self.runCommand(command)
+
+        command = pvs_values["command"][1] % "GA:1,2,3"
+
+        result = self.runCommand(command)
+
+        if not result:
+            self.vim.out_write("Error with %s\n" % current_file)
+            return
+
+        result = result.stdout.decode("utf-8").split('\n')
+
+        errors = "\n".join([x for x in result if current_file in x])
+
+        self.writeToQuickFix(errors, pvs_values["tempFile"])
 
     @neovim.command("AnalysisOclint", range='', nargs='*')
-    def analysisOclint(self):
+    def analysis_oclint(self, arg, range):
         pass
 
     @neovim.command("AnalysisCppcheck", range='', nargs='*')
-    def analysisCppcheck(self):
-        pass
+    def analysis_cpp_check(self, arg, range):
+        if not self.check_build_dir():
+            return
+
+        current_file = self.vim.current.buffer.name
+
+        command = self.commands["cppcheck"]
+
+        if command["needBuild"]:
+            chdir(self.build_dir)
+
+        result = self.runCommand(command["command"][0] % current_file)
+
+        if not result:
+            self.vim.out_write("Error with %s\n" % current_file)
+            return
+
+        temp_buffer = result.stderr.decode("utf-8").split('\n')
+
+        result = []
+        for line in temp_buffer:
+            if current_file in line:
+                temp = line.split('[')[1].split(']')
+                file_name_in_cppcheck_format = temp[0]
+                message = temp[1][2:]
+                result.append(file_name_in_cppcheck_format + ":0: " + message)
+
+        errors = "\n".join(result)
+
+        self.writeToQuickFix(errors, command["tempFile"])
 
     def writeToQuickFix(self, message, tempFile):
-        with open(tempFile, 'w') as f:
-            f.write(message)
+        with open(tempFile, 'w') as file_handler:
+            file_handler.write(message)
         self.vim.command("cfile %s" % tempFile)
         self.vim.command("copen")
 
@@ -111,11 +141,30 @@ class Main(object):
                 check=True,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE)
-            if (result.returncode != 0):
+            if result.returncode != 0:
                 self.vim.err_write("ERROR at {}:\n{}\n{}".format(
                     result.args, result.stderr.decode("utf-8"),
                     result.stdout.decode("utf-8")))
                 return None
             return result
-        except subprocess.CalledProcessError as e:
-            self.vim.err_write(e.output)
+        except subprocess.CalledProcessError as expt:
+            self.vim.err_write(expt.output)
+
+    def check_build_dir(self):
+        if (not self.build_dir or not exists(self.build_dir)):
+            if exists('build/'):
+                if exists('build/compile_commands.json'):
+                    self.build_dir = 'build/'
+                    return True
+                elif exists('Debug/'):
+                    self.build_dir = "build/Debug/"
+                    return True
+                elif exists('Release/'):
+                    self.build_dir = "build/Release/"
+                    return True
+
+            self.vim.out_write("Please set Build dir\n")
+            self.vim.out_write("Using :SetBuild \"build\"\n")
+            return False
+        else:
+            return True
