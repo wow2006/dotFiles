9c08e878da890a207c5ae0295ff99835842abdc5
Ahmed Hossain Abdelaal Ali <ahmhali.c@tahakom.com>
2026-04-12 11:28:04 +0300

    Refine the nvim plugin

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,7 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
+    vim.opt.termguicolors = true
     require("bufferline").setup{}
   end
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,12 +2,13 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-fzf-native.nvim',
   },
   lazy=false,
   tag='0.1.8',
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<C-P>', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<C-h>', builtin.help_tags, { desc = 'Telescope help tags' })
   end
